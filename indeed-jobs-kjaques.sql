4c616709bce97deafed910134bec799adeb7b50e
kjaques <jaques.kyle@gmail.com>
2025-07-24 15:08:53 -0400

    Project Complete - answers updated

--- indeed-jobs-kjaques.sql
+++ indeed-jobs-kjaques.sql
@@ -3,7 +3,7 @@ FROM data_analyst_jobs;
 
 --     1. How many rows are in the data_analyst_jobs table?
 -- 1793
-SELECT COUNT(title)
+SELECT COUNT(*)
 FROM data_analyst_jobs;
 
 --     2. Write a query to look at just the first 10 rows. What company is associated with the job posting on the 10th row?
@@ -25,13 +25,13 @@ WHERE location = 'TN'
 
 --     4. How many postings in Tennessee have a star rating above 4?
 -- 3
-SELECT COUNT(title)
+SELECT COUNT(*)
 from data_analyst_jobs
 WHERE location = 'TN' AND star_rating > 4;
 
 --     5. How many postings in the dataset have a review count between 500 and 1000?
 -- 150
-SELECT COUNT(title)
+SELECT COUNT(*)
 from data_analyst_jobs
 WHERE review_count > 500 AND review_count < 1000;
 
@@ -47,16 +47,16 @@ ORDER BY avg_rating DESC;
 --     7. Select unique job titles from the data_analyst_jobs table. How many are there?
 -- 881
 SELECT COUNT(DISTINCT title)
-from data_analyst_jobs;
+FROM data_analyst_jobs;
 
 --     8. How many unique job titles are there for California companies?
 -- 230
 SELECT COUNT(DISTINCT title)
-from data_analyst_jobs
+FROM data_analyst_jobs
 WHERE location = 'CA';
 
 --     9. Find the name of each company and its average star rating for all companies that have more than 5000 reviews across all locations. How many companies are there with more that 5000 reviews across all locations?
--- 71
+-- 71 (or 40)
 SELECT company, 
 	SUM(review_count) AS num_review, 
 	AVG(star_rating) AS avg_rating
@@ -65,16 +65,11 @@ WHERE company is NOT NULL AND star_rating is NOT NULL
 GROUP BY company
 HAVING SUM(review_count) > 5000;
 
--- SELECT company, 
--- 	AVG(CASE WHEN review_count > 5000 THEN star_rating END) AS avg_rating
--- FROM data_analyst_jobs
--- WHERE company is NOT NULL AND star_rating is NOT NULL AND review_count > 5000
--- GROUP BY company
--- ORDER BY avg_rating DESC;
-
-SELECT company, review_count, star_rating
+SELECT company, 
+ 	AVG(star_rating) AS avg_rating
 FROM data_analyst_jobs
-WHERE company = 'Google';
+WHERE company is NOT NULL AND star_rating is NOT NULL AND review_count > 5000
+GROUP BY company;
 
 --     10. Add the code to order the query in #9 from highest to lowest average star rating. Which company with more than 5000 reviews across all locations in the dataset has the highest star rating? What is that rating?
 -- Google, 4.3
@@ -85,7 +80,8 @@ FROM data_analyst_jobs
 WHERE company is NOT NULL AND star_rating is NOT NULL
 GROUP BY company
 HAVING SUM(review_count) > 5000
-ORDER BY avg_rating DESC;
+ORDER BY avg_rating DESC
+LIMIT 1;
 
 --     11. Find all the job titles that contain the word ‘Analyst’. How many different job titles are there?
 -- 774
@@ -94,7 +90,7 @@ from data_analyst_jobs
 WHERE title ILIKE '%analyst%';
 
 --     12. How many different job titles do not contain either the word ‘Analyst’ or the word ‘Analytics’? What word do these positions have in common?
--- 4
+-- 4, Tableau
 SELECT COUNT(DISTINCT title)
 from data_analyst_jobs
 WHERE title NOT ILIKE '%analyst%' AND title NOT ILIKE '%analytics%';
